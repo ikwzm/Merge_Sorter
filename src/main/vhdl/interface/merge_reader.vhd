c92fd022610e1c55c586da9cbf67a8ff06a9db9d
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2020-09-29 15:03:22 +0900

    [fix] the wrong length of I_STAT_RESV_NULL in Merge_Reader/Merge_Writer

--- src/main/vhdl/interface/merge_reader.vhd
+++ src/main/vhdl/interface/merge_reader.vhd
@@ -151,7 +151,7 @@ architecture RTL of Merge_Reader is
                                     := std_logic_vector(to_unsigned(2**BUF_DEPTH-2**MAX_XFER_SIZE   , BUF_DEPTH+1));
     constant  I_BUF_READY_LEVEL     :  std_logic_vector(BUF_DEPTH downto 0)
                                     := std_logic_vector(to_unsigned(2**BUF_DEPTH-2*(BUF_DATA_BITS/8), BUF_DEPTH+1));
-    constant  I_STAT_RESV_NULL      :  std_logic_vector(REG_PARAM.STAT_RESV_BITS downto 0) := (others => '0');
+    constant  I_STAT_RESV_NULL      :  std_logic_vector(REG_PARAM.STAT_RESV_BITS-1 downto 0) := (others => '0');
     -------------------------------------------------------------------------------
     -- データバスのビット数の２のべき乗値を計算する.
     -------------------------------------------------------------------------------
--- src/main/vhdl/interface/merge_writer.vhd
+++ src/main/vhdl/interface/merge_writer.vhd
@@ -146,7 +146,7 @@ architecture RTL of Merge_Writer is
                                     := std_logic_vector(to_unsigned(2**MAX_XFER_SIZE   , BUF_DEPTH+1));
     constant  O_BUF_READY_LEVEL     :  std_logic_vector(BUF_DEPTH downto 0)
                                     := std_logic_vector(to_unsigned(2*(BUF_DATA_BITS/8), BUF_DEPTH+1));
-    constant  O_STAT_RESV_NULL      :  std_logic_vector(REG_PARAM.STAT_RESV_BITS downto 0) := (others => '0');
+    constant  O_STAT_RESV_NULL      :  std_logic_vector(REG_PARAM.STAT_RESV_BITS-1 downto 0) := (others => '0');
     -------------------------------------------------------------------------------
     -- データバスのビット数の２のべき乗値を計算する.
     -------------------------------------------------------------------------------
