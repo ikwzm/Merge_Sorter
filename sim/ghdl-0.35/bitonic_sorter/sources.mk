8cacf2da6b798afee2d40623524d08f5836ac93d
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2022-10-22 18:32:21 +0900

    [update] sim/ghdl-0.35

--- sim/ghdl-0.35/bitonic_sorter/sources.mk
+++ sim/ghdl-0.35/bitonic_sorter/sources.mk
@@ -1,8 +1,8 @@
+sorting_network.o : ../../../src/main/vhdl/core/sorting_network.vhd 
+	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network.vhd
+
 word.o : ../../../src/main/vhdl/core/word.vhd 
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/word.vhd
-
-sorting_network.o : ../../../src/main/vhdl/core/sorting_network.vhd word.o
-	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network.vhd
 
 core_components.o : ../../../src/main/vhdl/core/core_components.vhd word.o sorting_network.o
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/core_components.vhd
@@ -13,10 +13,13 @@ word_compare.o : ../../../src/main/vhdl/core/word_compare.vhd word.o
 word_pipeline_register.o : ../../../src/main/vhdl/core/word_pipeline_register.vhd word.o
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/word_pipeline_register.vhd
 
+bitonic_sorting_network.o : ../../../src/main/vhdl/core/bitonic_sorting_network.vhd sorting_network.o
+	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/bitonic_sorting_network.vhd
+
 sorting_network_core.o : ../../../src/main/vhdl/core/sorting_network_core.vhd word.o sorting_network.o core_components.o word_compare.o word_pipeline_register.o
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network_core.vhd
 
-bitonic_sorter.o : ../../../src/main/vhdl/examples/bitonic_sorter/bitonic_sorter.vhd word.o sorting_network.o core_components.o sorting_network_core.o
+bitonic_sorter.o : ../../../src/main/vhdl/examples/bitonic_sorter/bitonic_sorter.vhd word.o sorting_network.o bitonic_sorting_network.o core_components.o sorting_network_core.o
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/examples/bitonic_sorter/bitonic_sorter.vhd
 
 bitonic_sorter_test_bench.o : ../../../src/test/vhdl/bitonic_sorter_test_bench.vhd word.o bitonic_sorter.o
--- sim/ghdl-0.35/merge_sorter_tree/sources.mk
+++ sim/ghdl-0.35/merge_sorter_tree/sources.mk
@@ -1,8 +1,8 @@
+sorting_network : ../../../src/main/vhdl/core/sorting_network.vhd 
+	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network.vhd
+
 word : ../../../src/main/vhdl/core/word.vhd 
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/word.vhd
-
-sorting_network : ../../../src/main/vhdl/core/sorting_network.vhd word
-	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network.vhd
 
 core_components : ../../../src/main/vhdl/core/core_components.vhd word sorting_network
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/core_components.vhd
@@ -13,13 +13,16 @@ word_compare : ../../../src/main/vhdl/core/word_compare.vhd word
 word_pipeline_register : ../../../src/main/vhdl/core/word_pipeline_register.vhd word
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/word_pipeline_register.vhd
 
+oddeven_sorting_network : ../../../src/main/vhdl/core/oddeven_sorting_network.vhd sorting_network
+	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/oddeven_sorting_network.vhd
+
 sorting_network_core : ../../../src/main/vhdl/core/sorting_network_core.vhd word sorting_network core_components word_compare word_pipeline_register
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/sorting_network_core.vhd
 
 word_queue : ../../../src/main/vhdl/core/word_queue.vhd word
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/word_queue.vhd
 
-merge_sorter_node : ../../../src/main/vhdl/core/merge_sorter_node.vhd word sorting_network core_components word_compare word_queue sorting_network_core
+merge_sorter_node : ../../../src/main/vhdl/core/merge_sorter_node.vhd word sorting_network oddeven_sorting_network core_components word_compare word_queue sorting_network_core
 	ghdl -a -C $(GHDLFLAGS) --work=MERGE_SORTER ../../../src/main/vhdl/core/merge_sorter_node.vhd
 
 merge_sorter_tree : ../../../src/main/vhdl/core/merge_sorter_tree.vhd word core_components merge_sorter_node word_queue
