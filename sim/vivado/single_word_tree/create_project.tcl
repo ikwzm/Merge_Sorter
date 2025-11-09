921cc0dfa4afedf9cfc63ec5e04a504aa6488cd2
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-11-10 05:11:19 +0900

    [update] sim/vivado/single_word_tree

--- sim/vivado/single_word_tree/create_project.tcl
+++ sim/vivado/single_word_tree/create_project.tcl
@@ -85,7 +85,9 @@ set_property "top" "Merge_Sorter_Tree"  $obj
 # Set 'sim_1' fileset properties
 #
 set current_vivado_version [version -short]
-if       { [string first "2019.2" $current_vivado_version ] == 0 } {
+if       { [string first "2025.1" $current_vivado_version ] == 0 } {
+    set scenario_full_path [file join ".." ".." ".."      $scenario_file ]
+} elseif { [string first "2019.2" $current_vivado_version ] == 0 } {
     set scenario_full_path [file join ".." ".." ".."      $scenario_file ]
 } elseif { [string first "2018.3" $current_vivado_version ] == 0 } {
     set scenario_full_path [file join ".." ".." ".."      $scenario_file ]
