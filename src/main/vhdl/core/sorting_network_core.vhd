7f13673b3689fd1340f1ccc65ee9ee6a8d46fdc0
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2026-01-02 17:06:39 +0900

    [update] Core version to 1.6.2

--- src/main/vhdl/core/sorting_network_core.vhd
+++ src/main/vhdl/core/sorting_network_core.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    sorting_network_core.vhd
 --!     @brief   Sorting Network Core Module :
---!     @version 1.6.0
---!     @date    2025/5/26
+--!     @version 1.6.2
+--!     @date    2026/1/2
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2020-2025 Ichiro Kawazome
+--      Copyright (C) 2020-2026 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
