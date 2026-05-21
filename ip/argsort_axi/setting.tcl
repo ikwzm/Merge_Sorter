c01ee7c99b0e9ac4c4e0adab4847551e69ce2d67
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2026-05-21 10:20:39 +0900

    [update] script files for build ip/argsort_axi_1.7

--- ip/argsort_axi/setting.tcl
+++ ip/argsort_axi/setting.tcl
@@ -3,7 +3,7 @@
 #
 set ip_name                 "ArgSort_AXI"
 set ip_version              "1.7"
-set ip_core_revision        0
+set ip_core_revision        1
 set ip_vendor_name          "ikwzm"
 set ip_description          "ArgSort_AXI"
 set ip_library_name         "Merge_Sorter"
